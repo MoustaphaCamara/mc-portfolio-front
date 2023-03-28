42705422c5cd3a1654510957bfd4108818f46bd5
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-03-28 17:53:36 +0200

    vertical timeline

--- front-end/src/container/Experiences/Experiences.jsx
+++ front-end/src/container/Experiences/Experiences.jsx
@@ -5,111 +5,77 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { TbCircleLetterB } from "react-icons/tb";
-import { AppWrap, MotionWrap } from "../../wrapper";
-import { urlFor, client } from "../../client";
+import { BsPersonWorkspace } from "react-icons/bs";
+import { IoLogoReact } from "react-icons/io5";
+import { GiRobe } from "react-icons/gi";
+import { MdVaccines } from "react-icons/md";
+import { MotionWrap } from "../../wrapper";
+import { client } from "../../client";
 import "./Experiences.scss";
-import { images } from "../../constants";
+// import { images } from "../../constants";
 
 const Experiences = () => {
   const [experiences, setExperiences] = useState([]);
 
   useEffect(() => {
-    const query = "*[_type == 'experiences']";
-    // get Experiencs
+    const query = "*[_type == 'experiences'] | order(year desc)";
     client.fetch(query).then((data) => {
       setExperiences(data);
+      console.log(data);
     });
   }, []);
 
   return (
-    <div>
-      {/* EXPERIENCES ENTREPRISE */}
+    <div id="experiences">
+      <h2 className="head-text">Expériences</h2>
       <motion.div className="app__skills-exp">
         <VerticalTimeline>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            contentArrowStyle={{
-              borderRight: "7px solid  rgb(33, 150, 243)",
-            }}
-            date="2011 - present"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<TbCircleLetterB />}
-            iconClassName={"test"}
-          >
-            <h3 className="vertical-timeline-element-title">bsOft.fr</h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Développeur web
-            </h4>
-            <ul>
-              <li>
-                Création d'interface utilisateur (React/TypeScript) avec Yeoman
-              </li>
-              <li>Intégration d'applications via API</li>
-              <li>
-                POC : application d'onglet dans Microsoft Teams (avec SSO)
-              </li>
-              <li>
-                Rédaction documentation & spécifications pour maintenabilité du
-                projet
-              </li>
-              <li>Création & intégration d'un bot discord en NodeJs/Express</li>
-            </ul>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            contentArrowStyle={{
-              borderRight: "7px solid  rgb(33, 150, 243)",
-            }}
-            date="2011 - present"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<TbCircleLetterB />}
-            iconClassName={"test"}
-          >
-            <h3 className="vertical-timeline-element-title">bsOft.fr</h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Développeur web
-            </h4>
-            <ul>
-              <li>
-                Création d'interface utilisateur (React/TypeScript) avec Yeoman
-              </li>
-              <li>Intégration d'applications via API</li>
-              <li>
-                POC : application d'onglet dans Microsoft Teams (avec SSO)
-              </li>
-              <li>
-                Rédaction documentation & spécifications pour maintenabilité du
-                projet
-              </li>
-              <li>Création & intégration d'un bot discord en NodeJs/Express</li>
-            </ul>
-          </VerticalTimelineElement>
+          {experiences.map((experience, index) => (
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{
+                background: "var(--purple-color)",
+                color: "#fff",
+              }}
+              contentArrowStyle={{
+                borderRight: "16px solid  var(--purple-color)",
+              }}
+              date={experience.year}
+              iconStyle={{
+                background: "var(--light-purple-color)",
+                color: "#fff",
+              }}
+              icon={
+                experience.company === "bsOft.fr" ? (
+                  <IoLogoReact />
+                ) : experience.company === "Ponsard & Dumas" ? (
+                  <GiRobe />
+                ) : experience.company === "MSD Vaccins" ? (
+                  <MdVaccines />
+                ) : (
+                  <BsPersonWorkspace />
+                )
+              }
+              iconClassName={"test"}
+            >
+              {/* content */}
+              <div>
+                <motion.div
+                  whileInView={{ opacity: [0, 1] }}
+                  transition={{ duration: 1.5 }}
+                  key={index}
+                >
+                  <h3 className="vertical-timeline-element-title">
+                    {experience.company}
+                  </h3>
+                  <h4 className="bold-text">{experience.occupation}</h4>
+                  <p className="p-text">{experience.description}</p>
+                </motion.div>
+                {/* end of content */}
+              </div>
+            </VerticalTimelineElement>
+          ))}
         </VerticalTimeline>
-        {experiences?.map((experience) => (
-          <motion.div className="app__skills-exp-item" key={experience.year}>
-            <div className="app__skills-exp-year">
-              <p className="bold-text">{experience.year}</p>
-            </div>
-            <motion.div className="app__skills-exp-works">
-              {experience.experience.map((work) => (
-                <>
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    transition={{ duration: 0.5 }}
-                    className="app__skills-exp-work"
-                    key={work.name}
-                  >
-                    <h4 className="bold-text">{work.name}</h4>
-                    <p className="p-text">{work.company}</p>
-                  </motion.div>
-                </>
-              ))}
-            </motion.div>
-          </motion.div>
-        ))}
       </motion.div>
     </div>
   );
