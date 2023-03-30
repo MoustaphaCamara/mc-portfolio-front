6d8418f89c909ca0861e5ca69ee96ad0c164d1f9
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-03-30 02:16:33 +0200

    client

--- src/client.js
+++ src/client.js
@@ -1,6 +1,5 @@
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
-
 export const client = createClient({
   projectId: import.meta.env.VITE_REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
@@ -8,7 +7,6 @@ export const client = createClient({
   useCdn: true,
   token: import.meta.env.VITE_REACT_APP_SANITY_TOKEN,
 });
-
 const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);
