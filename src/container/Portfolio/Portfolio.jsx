b3328fd5e56cb95a4552bdcb752069889e9aa9cb
Moustapha <cmr.mous@gmail.com>
2023-05-30 14:21:18 +0200

    useFetch hook & data filtering

--- src/container/Portfolio/Portfolio.jsx
+++ src/container/Portfolio/Portfolio.jsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 import Slider from "react-slick";
 
 import { MotionWrap } from "../../wrapper";
-import { urlFor, client } from "../../client";
+import { urlFor } from "../../client";
 
 import "./Portfolio.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import useFetch from "../../hooks/useFetch";
 
 const settings = {
   dots: true,
@@ -31,37 +32,23 @@ const filterButtons = [
 const Portfolio = () => {
   const [activeFilter, setActiveFilter] = useState("Tout afficher");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
-  const [works, setWorks] = useState([]);
-  const [filterWork, setFilterWork] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  // default filtering doesn't display all data on load
 
-  useEffect(() => {
-    const query = "*[_type == 'works'] | order(releaseDate desc)";
-
-    async function fetchData() {
-      try {
-        const response = await client.fetch(query);
-        setWorks(response);
-        setFilterWork(response);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        console.log("fetched");
-      }
-    }
-    fetchData();
-  }, []);
-
+  const { data, loading, error } = useFetch(
+    "*[_type == 'works'] | order(releaseDate desc)"
+  );
+  if (error) console.log(error);
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
     setAnimateCard([{ y: 100, opacity: 0 }]);
 
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
-
       if (item === "Tout afficher") {
-        setFilterWork(works);
+        setFilteredData(data);
       } else {
-        setFilterWork(works.filter((work) => work.tags.includes(item)));
+        setFilteredData(data.filter((work) => work.tags.includes(item)));
       }
     }, 500);
   };
@@ -87,56 +74,58 @@ const Portfolio = () => {
       </div>
 
       <Slider {...settings}>
-        {filterWork.map((work, index) => (
-          <motion.div
-            key={index}
-            animate={animateCard}
-            transition={{
-              duration: 0.5,
-              delayChildren: 0.5,
-            }}
-          >
-            <div className="app__portfolio-item" key={index}>
-              <div className="app__portfolio-item-picture-container">
-                <div className="app__portfolio-item-picture">
-                  <a href={work.projectLink} target="_blank" rel="noreferrer">
-                    <img src={urlFor(work.imgUrl)} alt={work.name} />
-                  </a>
+        {data &&
+          filteredData &&
+          filteredData.map((work, index) => (
+            <motion.div
+              key={index}
+              animate={animateCard}
+              transition={{
+                duration: 0.5,
+                delayChildren: 0.5,
+              }}
+            >
+              <div className="app__portfolio-item" key={index}>
+                <div className="app__portfolio-item-picture-container">
+                  <div className="app__portfolio-item-picture">
+                    <a href={work.projectLink} target="_blank" rel="noreferrer">
+                      <img src={urlFor(work.imgUrl)} alt={work.name} />
+                    </a>
+                  </div>
+                </div>
+                <div className="app__portfolio-item-description">
+                  <h4 className="head-text">{work.title}</h4>
+                  <p style={{ marginTop: 10 }}>{work.description}</p>
+                  <p style={{ color: "var(--orange-color)" }}>
+                    Technologies utilisées :
+                  </p>
+                  <div className="app__portfolio-icons">
+                    {work.icon.map((icon, index) => (
+                      <img src={urlFor(icon)} alt={work.name} key={index} />
+                    ))}
+                  </div>
+                  <div className="app__portfolio-links">
+                    <a
+                      href={work.sourceCode}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="app__portfolio-link-item"
+                    >
+                      <AiFillGithub /> Code source
+                    </a>
+                    <a
+                      href={work.projectLink}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="app__portfolio-link-item"
+                    >
+                      <AiFillEye /> Visiter
+                    </a>
+                  </div>
                 </div>
               </div>
-              <div className="app__portfolio-item-description">
-                <h4 className="head-text">{work.title}</h4>
-                <p style={{ marginTop: 10 }}>{work.description}</p>
-                <p style={{ color: "var(--orange-color)" }}>
-                  Technologies utilisées :
-                </p>
-                <div className="app__portfolio-icons">
-                  {work.icon.map((icon, index) => (
-                    <img src={urlFor(icon)} alt={work.name} key={index} />
-                  ))}
-                </div>
-                <div className="app__portfolio-links">
-                  <a
-                    href={work.sourceCode}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="app__portfolio-link-item"
-                  >
-                    <AiFillGithub /> Code source
-                  </a>
-                  <a
-                    href={work.projectLink}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="app__portfolio-link-item"
-                  >
-                    <AiFillEye /> Visiter
-                  </a>
-                </div>
-              </div>
-            </div>
-          </motion.div>
-        ))}
+            </motion.div>
+          ))}
       </Slider>
     </div>
   );
