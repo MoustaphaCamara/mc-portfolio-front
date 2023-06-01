c2c028d1b18f234ed130545289786314a0c72670
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-06-01 13:27:10 +0200

    useFetch hook and correcting filter/rename & remove useless active state

--- src/container/Portfolio/Portfolio.jsx
+++ src/container/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 import Slider from "react-slick";
@@ -30,26 +30,27 @@ const filterButtons = [
 ];
 
 const Portfolio = () => {
-  const [activeFilter, setActiveFilter] = useState("Tout afficher");
+  const [filter, setFilter] = useState("Tout afficher");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
-  const [filteredData, setFilteredData] = useState([]);
-  // default filtering doesn't display all data on load
-
-  const { data, loading, error } = useFetch(
-    "*[_type == 'works'] | order(releaseDate desc)"
-  );
+  const [query, setQuery] = useState();
+  const { data, loading, error } = useFetch(query);
   if (error) console.log(error);
-  const handleWorkFilter = (item) => {
-    setActiveFilter(item);
-    setAnimateCard([{ y: 100, opacity: 0 }]);
 
+  useEffect(() => {
+    if (filter === "Tout afficher") {
+      setQuery(`*[_type == "works"] | order(releaseDate desc)`);
+    } else {
+      setQuery(
+        `*[_type == "works" && "${filter}" in tags] | order(releaseDate desc)`
+      );
+    }
+  }, [filter]);
+
+  const handleWorkFilter = (item) => {
+    setFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
-      if (item === "Tout afficher") {
-        setFilteredData(data);
-      } else {
-        setFilteredData(data.filter((work) => work.tags.includes(item)));
-      }
     }, 500);
   };
 
@@ -60,72 +61,68 @@ const Portfolio = () => {
       </h2>
 
       <div className="app__portfolio-filter">
-        {filterButtons.map((filter, index) => (
+        {filterButtons.map((item, index) => (
           <div
             key={index}
-            onClick={() => handleWorkFilter(filter)}
-            className={`app__btn ${
-              activeFilter === filter ? "app__btn-active" : ""
-            }`}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__btn ${filter === item ? "app__btn-active" : ""}`}
           >
-            {filter}
+            {item}
           </div>
         ))}
       </div>
 
       <Slider {...settings}>
-        {data &&
-          filteredData &&
-          filteredData.map((work, index) => (
-            <motion.div
-              key={index}
-              animate={animateCard}
-              transition={{
-                duration: 0.5,
-                delayChildren: 0.5,
-              }}
-            >
-              <div className="app__portfolio-item" key={index}>
-                <div className="app__portfolio-item-picture-container">
-                  <div className="app__portfolio-item-picture">
-                    <a href={work.projectLink} target="_blank" rel="noreferrer">
-                      <img src={urlFor(work.imgUrl)} alt={work.name} />
-                    </a>
-                  </div>
-                </div>
-                <div className="app__portfolio-item-description">
-                  <h4 className="head-text">{work.title}</h4>
-                  <p style={{ marginTop: 10 }}>{work.description}</p>
-                  <p style={{ color: "var(--orange-color)" }}>
-                    Technologies utilisées :
-                  </p>
-                  <div className="app__portfolio-icons">
-                    {work.icon.map((icon, index) => (
-                      <img src={urlFor(icon)} alt={work.name} key={index} />
-                    ))}
-                  </div>
-                  <div className="app__portfolio-links">
-                    <a
-                      href={work.sourceCode}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="app__portfolio-link-item"
-                    >
-                      <AiFillGithub /> Code source
-                    </a>
-                    <a
-                      href={work.projectLink}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="app__portfolio-link-item"
-                    >
-                      <AiFillEye /> Visiter
-                    </a>
-                  </div>
+        {data?.map((work, index) => (
+          <motion.div
+            key={index}
+            animate={animateCard}
+            transition={{
+              duration: 0.5,
+              delayChildren: 0.5,
+            }}
+          >
+            <div className="app__portfolio-item" key={index}>
+              <div className="app__portfolio-item-picture-container">
+                <div className="app__portfolio-item-picture">
+                  <a href={work.projectLink} target="_blank" rel="noreferrer">
+                    <img src={urlFor(work.imgUrl)} alt={work.name} />
+                  </a>
                 </div>
               </div>
-            </motion.div>
-          ))}
+              <div className="app__portfolio-item-description">
+                <h4 className="head-text">{work.title}</h4>
+                <p style={{ marginTop: 10 }}>{work.description}</p>
+                <p style={{ color: "var(--orange-color)" }}>
+                  Technologies utilisées :
+                </p>
+                <div className="app__portfolio-icons">
+                  {work.icon.map((icon, index) => (
+                    <img src={urlFor(icon)} alt={work.name} key={index} />
+                  ))}
+                </div>
+                <div className="app__portfolio-links">
+                  <a
+                    href={work.sourceCode}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="app__portfolio-link-item"
+                  >
+                    <AiFillGithub /> Code source
+                  </a>
+                  <a
+                    href={work.projectLink}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="app__portfolio-link-item"
+                  >
+                    <AiFillEye /> Visiter
+                  </a>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        ))}
       </Slider>
     </div>
   );
