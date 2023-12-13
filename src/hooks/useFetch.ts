81274c1de5332277620019262e12ace4010a1efc
Moustapha Camara <cmr.mous@gmail.com>
2023-12-13 23:54:07 +0100

    refactor(typescript): migrate usefetch hook to ts

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,24 +1,56 @@
 import { useEffect, useState } from "react";
 import { client } from "../client.ts";
 
-export default function useFetch(query):{data: null | object, error: null |  object, loading: null |  false} {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    (async function () {
+export type ApiResponse = {
+  status: Number;
+  statusText: String;
+  data: any;
+  error: Error;
+  loading: Boolean;
+};
+
+export default function useFetch (query: string):ApiResponse {
+  const [status, setStatus] = useState<Number>(0);
+  const [statusText, setStatusText] = useState<String>('');
+  const [data, setData] = useState<any>();
+  const [error, setError] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(false);
+    const getData = async ():Promise<void> => {
+      setLoading(true);
       try {
-        setLoading(true);
-        const response = await client.fetch(query);
-        setData(response);
+        const apiResponse = await client  .fetch(query);
+        console.log(apiResponse)
+        // setStatus(apiResponse.status)
+        // setData(apiResponse);
       } catch (err) {
-        setError(err);
+        setError(err)
       } finally {
         setLoading(false);
       }
-    })();
-    //https://usefulangle.com/post/248/javascript-async-anonymous-function-iife
-  }, [query]);
+    }
 
-  return { data, error, loading };
+  useEffect(() => {
+    getData();
+  }, []);
+  return {status, statusText, data, error,loading};
 }
+// export default function useFetch2(query):{data: null | object, error: null |  object, loading: null |  false} {
+//   const [data, setData] = useState(null);
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   useEffect(() => {
+//     (async function () {
+//       try {
+//         setLoading(true);
+//         const response = await client.fetch(query);
+//         setData(response);
+//       } catch (err) {
+//         setError(err);
+//       } finally {
+//         setLoading(false);
+//       }
+//     })();
+//     //https://usefulangle.com/post/248/javascript-async-anonymous-function-iife
+//   }, [query]);
+//   return { data, error, loading };
+// }
