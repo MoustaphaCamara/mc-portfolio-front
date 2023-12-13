a1204ec045b2b852ef831c5aa8a28a567e9457f1
Moustapha Camara <cmr.mous@gmail.com>
2023-12-13 23:06:50 +0100

    refactor(typescript): update ts rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,29 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended'],
-  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2021,
+    ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
+  plugins: ['react-refresh'],
   rules: {
-    'prettier/prettier': 'error',
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
-};
+}
