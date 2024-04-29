c0cbb2138a2bf862f14dd801305df04d07b0b6c2
Moustapha Camara <cmr.mous@gmail.com>
2024-04-29 09:40:47 +0200

    fix(ci): conflict

--- src/App.tsx
+++ src/App.tsx
@@ -27,4 +27,3 @@ const App = () => {
 };
 
 export default App;
-//this pr goes boom
--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
-// const imageUrlBuilder = imageUrl;
-
 
 export const client = createClient({
   projectId: import.meta.env.VITE_REACT_APP_SANITY_PROJECT_ID,
