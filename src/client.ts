1669ac1673a493340aa3a3dd0a04a3d5e3bde21f
Moustapha Camara <cmr.mous@gmail.com>
2024-04-29 09:51:17 +0200

    fix(ci): build issue was due to typing client ts

--- src/client.ts
+++ src/client.ts
@@ -1,14 +1,12 @@
-import {createClient, SanityClient} from "@sanity/client";
-import * as imageUrl from "@sanity/image-url";
-import {ImageUrlBuilder} from "@sanity/image-url/lib/types/builder";
-const imageUrlBuilder = imageUrl;
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
-export const client: SanityClient = createClient({
+export const client = createClient({
   projectId: import.meta.env.VITE_REACT_APP_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_REACT_APP_SANITY_DATASET,
   apiVersion: import.meta.env.VITE_REACT_APP_SANITY_API_VERSION,
   useCdn: true,
 });
-const builder: ImageUrlBuilder = imageUrlBuilder(client);
+const builder = imageUrlBuilder(client);
 
-export const urlFor = (source : string ) => builder.image(source).toString();
+export const urlFor = (source) => builder.image(source);
