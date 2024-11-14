036ea7b9fa98a0f61c14ec2a7a2c501c8450a0a3
MoustaphaCamara <cmr.mous@gmail.com>
2024-11-14 23:35:30 +0100

    chore:files

--- src/constants/files.ts
+++ src/constants/files.ts
@@ -1,5 +1,5 @@
-import cv_fr from '../assets/FR-2024-07-CV_Moustapha_Camara.pdf';
-import cv_en from '../assets/EN-2024-07-CV_Moustapha_Camara.pdf';
+import cv_fr from '../assets/FR-2024-11-CV_Moustapha_Camara.pdf';
+import cv_en from '../assets/EN-2024-11-CV_Moustapha_Camara.pdf';
 
 export default {
   cv_fr,
