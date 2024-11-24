2f9df9231720e3e69f17c9d3b2ef9ce80da894d1
MoustaphaCamara <cmr.mous@gmail.com>
2024-11-24 23:35:54 +0100

    move to separate lang folders

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './i18n.js';
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import React from 'react';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.Suspense fallback="loading..">
+    <App />
+  </React.Suspense>
+);
