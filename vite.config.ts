5fde96f38d23bbd5c545a42a78a6fdb1d0cafa3c
MoustaphaCamara <cmr.mous@gmail.com>
2026-09-18 17:07:46 -0400

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mc-portfolio-front/',
   plugins: [react()],
   server: {
     port: parseInt(process.env.VITE_PORT || '5173'),
